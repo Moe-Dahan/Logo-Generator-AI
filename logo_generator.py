e71e6addae9a0a94c21daf6b16c2ec7f67e5aab5
Moe-Dahan <83793097+Moe-Dahan@users.noreply.github.com>
2025-01-08 12:46:54 +1100

    Add files via upload

---
+++ logo_generator.py
@@ -0,0 +1,24 @@
+import requests
+import io
+from PIL import Image
+
+
+API_URL = "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-dev"
+
+headers = {"Authorization": "Bearer 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", "x-wait-for-model" : "true", "x-use-cache": "false"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.content
+
+question = input("[+] Enter prompt for logo: ")
+print("[+] Creating Logo Please wait....")
+image_bytes = query({
+	"inputs": f"{question}",
+	"target_size" : 200,
+	"guidance_scale" : 50,
+})
+
+image = Image.open(io.BytesIO(image_bytes))
+image.save('logo.png')
+print("[+] Done Creating Logo saved in Dir....")
